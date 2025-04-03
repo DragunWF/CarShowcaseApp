e7773c0919c25bcaa2e1649e52ba6735d95d2753
DragunDev <plarisanmarc@gmail.com>
2025-04-03 15:42:58 +0800

    Implement categories for vehicles

--- app/src/main/java/com/example/carshowcaseapp/data/Vehicle.java
+++ app/src/main/java/com/example/carshowcaseapp/data/Vehicle.java
@@ -1,16 +1,18 @@
 package com.example.carshowcaseapp.data;
 
 public class Vehicle extends Model {
-    private String model, brand, features, imageName;
+    private String model, brand, features, category, imageName;
     private double price;
 
-    public Vehicle(String model, String brand, String features, String imageName, double price) {
+    public Vehicle(String model, String brand, String features, String category, String imageName, double price) {
         this.model = model;
         this.brand = brand;
         this.features = features;
+        this.category = category;
         this.imageName = imageName;
         this.price = price;
     }
+
 
     @Override
     public String toString() {
@@ -18,6 +20,8 @@ public class Vehicle extends Model {
                 "model='" + model + '\'' +
                 ", brand='" + brand + '\'' +
                 ", features='" + features + '\'' +
+                ", category='" + category + '\'' +
+                ", imageName='" + imageName + '\'' +
                 ", price=" + price +
                 ", id=" + id +
                 '}';
@@ -53,5 +57,21 @@ public class Vehicle extends Model {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
     }
 }
