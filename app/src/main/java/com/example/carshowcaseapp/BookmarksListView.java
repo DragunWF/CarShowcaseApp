bfd626d35ae9fc1793195d12bec72f441d5528a1
DragunDev <plarisanmarc@gmail.com>
2025-04-03 18:03:42 +0800

    Rename book mark list activity

--- app/src/main/java/com/example/carshowcaseapp/BookmarksListView.java
+++ app/src/main/java/com/example/carshowcaseapp/BookmarksListView.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BookmaksListView extends AppCompatActivity {
+public class BookmarksListView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
