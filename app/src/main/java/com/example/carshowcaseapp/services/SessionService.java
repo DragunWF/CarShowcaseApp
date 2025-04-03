2b25614a85494ba9a8d7fb1fc3b16c1a9dc779d7
DragunDev <plarisanmarc@gmail.com>
2025-04-03 15:40:45 +0800

    Implement additional services for session and bookmarking

--- app/src/main/java/com/example/carshowcaseapp/services/SessionService.java
+++ app/src/main/java/com/example/carshowcaseapp/services/SessionService.java
@@ -6,13 +6,13 @@ import com.example.carshowcaseapp.helpers.DatabaseHelper;
 import java.util.List;
 
 public class SessionService {
-    private int currentAdminId = -1; // -1 means no admin
+    private static int currentAdminId = -1; // -1 means no admin
 
-    public int getCurrentAdminId() {
+    public static int getCurrentAdminId() {
         return currentAdminId;
     }
 
-    public boolean login(String username, String password) {
+    public static boolean login(String username, String password) {
         List<Admin> admins = DatabaseHelper.getAdminBank().getAll();
         for (Admin admin : admins) {
             if (admin.getUsername().equals(username) && admin.getPassword().equals(password)) {
@@ -23,7 +23,7 @@ public class SessionService {
         return false;
     }
 
-    public void logout() {
+    public static void logout() {
         currentAdminId = -1;
     }
 }
